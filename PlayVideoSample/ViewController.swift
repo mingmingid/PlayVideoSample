1338b1161fb319505b8dd96f520dd9a97fc5b9ba
Oey King Ming <ming2_id@yahoo.com>
2019-07-15 11:07:22 +0700

    Initial Commit

---
+++ PlayVideoSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayVideoSample
+//
+//  Created by Oey King Ming on 15/07/19.
+//  Copyright © 2019 Oey King Ming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
